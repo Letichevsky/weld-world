94ab40760027b47ef07c736e05fb50385501668d
VL <vladislavletichevsky@gmail.com>
2024-09-23 17:04:41 +0200

    Switch brands pages was created

--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,49 +1,66 @@
-import aboutPageBG from '../assets/images/aboutPageBG.jpg'
-import olives from '../assets/images/olives.jpeg'
-import divaOlivaTin from '../assets/images/divaOlivaTin.png'
+import aboutPageBG from "../assets/images/aboutPageBG.jpg";
+import olives from "../assets/images/olives.jpeg";
+import divaOlivaTin from "../assets/images/divaOlivaTin.png";
 interface AboutUsPageProps {
   props?: string;
 }
 
-const AboutUsPage:React.FC<AboutUsPageProps> = ({props}) => {
+const AboutUsPage: React.FC<AboutUsPageProps> = ({ props }) => {
   return (
-    <div className='w-full'>
-      <img src={aboutPageBG} alt="our products" className='object-cover w-full max-h-[350px]' />
+    <div className="w-full">
+      <img
+        src={aboutPageBG}
+        alt="our products"
+        className="object-cover w-full max-h-[350px]"
+      />
       <div className="w-full h-[50px] bg-[#0f4b27]">
         <div className="max-w-[70svw] h-full mx-auto pl-[32px]">
-          <h1 className='text-white text-[32px]'>A rainbow of flavors on your table.</h1>
+          <h1 className="text-white text-[32px]">
+            A rainbow of flavors on your table.
+          </h1>
         </div>
       </div>
       <div className="w-full py-[50px]">
         <div className="w-full max-w-[70svw] mx-auto flex relative">
           <div className="bg-[#f7f7f7] w-[360px] min-w-[360px] p-8 flex flex-col gap-6 ">
-            <h2 className='text-[#0e4b25] text-[32px]'>Our history <br/> and achievements</h2>
+            <h2 className="text-[#0e4b25] text-[32px]">
+              Our history <br /> and achievements
+            </h2>
             <div className="h-[2px] w-[150px] bg-[#0e4b25]"></div>
-            <p className="text-[#696969]">Veld World was founded on December 9, 
-              2005 in the UK, starting its activity as a supplier of high-quality 
-              grocery group of goods. Canned pineapples and other fruits Tropic Life 
-              Dream Valley, olives and olives Diva Oliva and Dream Valley, 
-              as well as olive oil Diva Oliva - have already become loved by a huge 
-              number of con-sumers. Every day we try to develop the markets 
-              of Europe and Asia as much as possible. Striving to satisfy the taste 
-              needs of all our customers, we have expanded our range of meat and fish 
-              pre-serves. Also: chips, coffee, tea, nuts, water, fruit juices.</p>
+            <p className="text-[#696969]">
+              Veld World was founded on December 9, 2005 in the UK, starting its
+              activity as a supplier of high-quality grocery group of goods.
+              Canned pineapples and other fruits Tropic Life Dream Valley,
+              olives and olives Diva Oliva and Dream Valley, as well as olive
+              oil Diva Oliva - have already become loved by a huge number of
+              con-sumers. Every day we try to develop the markets of Europe and
+              Asia as much as possible. Striving to satisfy the taste needs of
+              all our customers, we have expanded our range of meat and fish
+              pre-serves. Also: chips, coffee, tea, nuts, water, fruit juices.
+            </p>
           </div>
-          <img src={olives} alt="olives" className='h-[600px] object-cover' />
-          <img src={divaOlivaTin} alt="Diva Oliva olives" className='absolute top-[-170px] left-[660px] h-[400px]' />
+          <img src={olives} alt="olives" className="h-[600px] object-cover" />
+          <img
+            src={divaOlivaTin}
+            alt="Diva Oliva olives"
+            className="absolute top-[-170px] left-[660px] h-[400px]"
+          />
         </div>
       </div>
       <div className="w-full bg-[#18713b] pl-[50%]">
         <div className="h-full max-w-[650px] bg-[#0f4b27] py-6 px-16">
-          <p className='text-white'>We constantly strive to ensure the highest quality 
-          of our products and maintain a high professional level of our services so that you can enjoy truly 
-          delicious and high-quality products that you love so much. Veld World is maximally focused on satisfying the needs 
-          of its customers. We are always ready for cooperation and devel-opment, 
-          promptly responding to changes in the market situation.</p>
+          <p className="text-white">
+            We constantly strive to ensure the highest quality of our products
+            and maintain a high professional level of our services so that you
+            can enjoy truly delicious and high-quality products that you love so
+            much. Veld World is maximally focused on satisfying the needs of its
+            customers. We are always ready for cooperation and devel-opment,
+            promptly responding to changes in the market situation.
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutUsPage
+export default AboutUsPage;
--- src/pages/BrandsPage.tsx
+++ src/pages/BrandsPage.tsx
@@ -1,53 +1,152 @@
-import brandsPageBG from '../assets/images/brandsPageBG.jpg'
-import divaOliva from '../assets/images/diva_oliva.svg'
-import dreamValley from '../assets/images/Dream_Valley.svg'
-import tropicLife from '../assets/images/tropic_life.svg'
-import veld from '../assets/images/veld.svg'
-import SimpleSlider from '../components/ProductsSlider'
+import { useState } from "react";
+import brandsPageBG from "../assets/images/brandsPageBG.jpg";
+import divaOliva from "../assets/images/diva_oliva.svg";
+import dreamValley from "../assets/images/Dream_Valley.svg";
+import tropicLife from "../assets/images/tropic_life.svg";
+import veld from "../assets/images/veld.svg";
+import SimpleSlider from "../components/ProductsSlider";
 
-interface BrandsPageProps {
-  props?: string;
-}
+const brandsData = [
+  {
+    logo: divaOliva,
+    title: "Diva Oliva",
+    description: `TM Diva Oliva presents a line of olive oil and olives, an affordable
+            Mediterranean delicacy, a tasty and healthy product. All products of
+            the Diva Oliva brand are manufactured at the best factories in
+            Italy, Spain, Morocco, Greece and Turkey and meet a high level of
+            quality. A wide assortment of TM Diva Oliva can satisfy the requests
+            of the most demanding gourmet. 
 
-const BrandsPage: React.FC<BrandsPageProps> = ({props}) => {
+            Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM
+            Diva Olliva, which literally means "country-side" deserves special
+            attention. This is the best unfiltered olive oil, the recipe of
+            which was passed down from generation to generation by one of the
+            oldest families in Italy. In 2011, the Veld World Itd company
+            acquired the right to the formulation and production of il Paesano
+            butter. The olive pulp inherent in this oll gives it a unique taste
+            and aroma, making it even more useful!`,
+  },
+  {
+    logo: dreamValley,
+    title: "Dream Valley",
+    description: `Dream Valley presents a line of olive oil and olives, an affordable
+            Mediterranean delicacy, a tasty and healthy product. All products of
+            the Diva Oliva brand are manufactured at the best factories in
+            Italy, Spain, Morocco, Greece and Turkey and meet a high level of
+            quality. A wide assortment of TM Diva Oliva can satisfy the requests
+            of the most demanding gourmet. 
+
+            Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM
+            Diva Olliva, which literally means "country-side" deserves special
+            attention. This is the best unfiltered olive oil, the recipe of
+            which was passed down from generation to generation by one of the
+            oldest families in Italy. In 2011, the Veld World Itd company
+            acquired the right to the formulation and production of il Paesano
+            butter. The olive pulp inherent in this oll gives it a unique taste
+            and aroma, making it even more useful!`,
+  },
+  {
+    logo: tropicLife,
+    title: "Tropic Life",
+    description: `Tropic Life presents a line of olive oil and olives, an affordable
+            Mediterranean delicacy, a tasty and healthy product. All products of
+            the Diva Oliva brand are manufactured at the best factories in
+            Italy, Spain, Morocco, Greece and Turkey and meet a high level of
+            quality. A wide assortment of TM Diva Oliva can satisfy the requests
+            of the most demanding gourmet. 
+
+            Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM
+            Diva Olliva, which literally means "country-side" deserves special
+            attention. This is the best unfiltered olive oil, the recipe of
+            which was passed down from generation to generation by one of the
+            oldest families in Italy. In 2011, the Veld World Itd company
+            acquired the right to the formulation and production of il Paesano
+            butter. The olive pulp inherent in this oll gives it a unique taste
+            and aroma, making it even more useful!`,
+  },
+  {
+    logo: veld,
+    title: "Veld World",
+    description: `Veld World presents a line of olive oil and olives, an affordable
+            Mediterranean delicacy, a tasty and healthy product. All products of
+            the Diva Oliva brand are manufactured at the best factories in
+            Italy, Spain, Morocco, Greece and Turkey and meet a high level of
+            quality. A wide assortment of TM Diva Oliva can satisfy the requests
+            of the most demanding gourmet. 
+
+            Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM
+            Diva Olliva, which literally means "country-side" deserves special
+            attention. This is the best unfiltered olive oil, the recipe of
+            which was passed down from generation to generation by one of the
+            oldest families in Italy. In 2011, the Veld World Itd company
+            acquired the right to the formulation and production of il Paesano
+            butter. The olive pulp inherent in this oll gives it a unique taste
+            and aroma, making it even more useful!`,
+  },
+];
+
+const BrandsPage: React.FC = () => {
+  const [selectedBrand, setSelectedBrand] = useState(brandsData[0]);
+
+  const handleBrandClick = (brand: (typeof brandsData)[number]) => {
+    setSelectedBrand(brand);
+
+    const element = document.getElementById("aboutBrand");
+    if (element) {
+      const offset = element.getBoundingClientRect().top + window.scrollY - 100;
+      window.scrollTo({
+        top: offset,
+        behavior: "smooth",
+      });
+    }
+  };
+
   return (
-    <div className='w-full'>
-      <img src={brandsPageBG} alt="Trees and vally" className='object-cover w-full' />
-      <div className="w-full">
+    <div className="w-full">
+      <img
+        src={brandsPageBG}
+        alt="Trees and vally"
+        className="object-cover w-full"
+      />
+      <div id="aboutBrand" className="w-full">
         <div className="w-full max-w-[70svw] mx-auto flex flex-col items-center gap-8 py-[50px]">
-          <img src={divaOliva} alt="Diva Oliva Logo" className='object-cover h-[190px] w-[400px]'/>
-          <p className='text-[#696969] max-w-[750px]'>TM Diva Oliva presents a line of olive oil and olives, an affordable Mediterranean 
-            delicacy, a tasty and healthy product. All products of the Diva Oliva brand are 
-            manufactured at the best factories in Italy, Spain, Morocco, Greece and Turkey and 
-            meet a high level of quality. A wide assortment of TM Diva Oliva can satisfy the 
-            requests of the most demanding gourmet. <br/><br/>
-          Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM Diva Olliva, which 
-          literally means "country-side" deserves special attention. This is the best unfiltered 
-          olive oil, the recipe of which was passed down from generation to generation by one of
-           the oldest families in Italy. In 2011, the Veld World Itd company acquired the right 
-           to the formulation and production of il Paesano butter. The olive pulp inherent in 
-           this oll gives it a unique taste and aroma, making it even more useful!</p>
+          <img
+            src={selectedBrand.logo}
+            alt={`${selectedBrand.title} Logo`}
+            className="object-cover h-[190px] w-auto"
+          />
+          <p className="text-[#696969] max-w-[750px]">
+            {selectedBrand.description}
+          </p>
         </div>
         <div className="w-full bg-[#18713b] h-[150px]">
           <div className="h-full max-w-[70svw] bg-[#f7f7f7] mx-auto px-16">
-            <SimpleSlider/>
+            <SimpleSlider />
           </div>
         </div>
         <div className="w-full">
           <div className="w-full max-w-[70svw] mx-auto flex flex-col items-center pt-8 gap-4">
             <h3 className="text-[32px]">Our brands</h3>
-            <p className="text-[#696969]">The company's distribution product portfolio includes the following brands:</p>
+            <p className="text-[#696969]">
+              The company's distribution product portfolio includes the
+              following brands:
+            </p>
             <div className="w-full flex justify-between">
-              <img src={divaOliva} alt="Diva Oliva logo" className='h-[200px] w-[200px]'/>
-              <img src={dreamValley} alt="Dream Valley logo" className='h-[200px] w-[200px]'/>
-              <img src={tropicLife} alt="Tropic Life logo" className='h-[200px] w-[200px]'/>
-              <img src={veld} alt="Veld World logo" className='h-[200px] w-[200px]'/>
+              {brandsData.map((brand, index) => (
+                <img
+                  key={index}
+                  src={brand.logo}
+                  alt={`${brand.title} logo`}
+                  className="h-[200px] w-[200px] hover:scale-[1.1] transition-all"
+                  onClick={() => handleBrandClick(brand)}
+                />
+              ))}
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BrandsPage
+export default BrandsPage;
