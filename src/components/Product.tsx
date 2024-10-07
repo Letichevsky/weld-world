4c45d34e0d7fb61b08e419726c32c7940c6c054d
VL <vladislavletichevsky@gmail.com>
2024-10-08 01:19:47 +0200

    added all products

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -8,10 +8,10 @@ const Product: React.FC<ProductProps> = ({ image, title, onClick }) => {
   return (
     <div
       onClick={onClick}
-      className="w-[200px] flex flex-col items-center gap-4 hover:shadow-md hover:scale-[1.1] py-4 transition-all cursor-pointer"
+      className="w-[200px] flex flex-col items-center gap-4  hover:scale-[1.1] py-4 transition-all cursor-pointer"
     >
-      <img src={image} alt="Product name" className="h-[200px]" />
-      <p className="text-[#696969] uppercase">{title}</p>
+      <img src={image} alt="Product name" className="w-[200px] h-[200px]" />
+      <p className="text-[#696969] uppercase text-center">{title}</p>
     </div>
   );
 };
