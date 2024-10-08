fecb8a446c2b7ae51b2a6ac2c337627793b68b3a
VL <vladislavletichevsky@gmail.com>
2024-10-08 12:01:59 +0200

    move brandsData from BrandsPage to data/brandsData

--- src/pages/BrandsPage.tsx
+++ src/pages/BrandsPage.tsx
@@ -1,13 +1,9 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import brandsPageBG from "../assets/images/brandsPageBG.jpg";
-import divaOliva from "../assets/images/diva_oliva.svg";
-import dreamValley from "../assets/images/Dream_Valley.svg";
-import tropicLife from "../assets/images/tropic_life.svg";
-import veld from "../assets/images/veld.svg";
 import SimpleSlider from "../components/ProductsSlider";
 import "./BrandsPage.css";
-
+import brandsData from "../data/brandsData";
 import { productCategories } from "../data/productsData";
 
 const productsFilter = (brand: string) => {
@@ -28,85 +24,6 @@ const dreamValleyProducts = productsFilter("Dream Valley");
 const divaOlivaProducts = productsFilter("Diva Oliva");
 const tropicLifeProducts = productsFilter("Tropic Life");
 const veldWorldProducts = productsFilter("Veld World");
-
-const brandsData = [
-  {
-    logo: divaOliva,
-    title: "Diva Oliva",
-    description: `TM Diva Oliva presents a line of olive oil and olives, an affordable
-              Mediterranean delicacy, a tasty and healthy product. All products of
-              the Diva Oliva brand are manufactured at the best factories in
-              Italy, Spain, Morocco, Greece and Turkey and meet a high level of
-              quality. A wide assortment of TM Diva Oliva can satisfy the requests
-              of the most demanding gourmet. 
-  
-              Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM
-              Diva Olliva, which literally means "country-side" deserves special
-              attention. This is the best unfiltered olive oil, the recipe of
-              which was passed down from generation to generation by one of the
-              oldest families in Italy. In 2011, the Veld World Itd company
-              acquired the right to the formulation and production of il Paesano
-              butter. The olive pulp inherent in this oll gives it a unique taste
-              and aroma, making it even more useful!`,
-  },
-  {
-    logo: dreamValley,
-    title: "Dream Valley",
-    description: `Dream Valley presents a line of olive oil and olives, an affordable
-              Mediterranean delicacy, a tasty and healthy product. All products of
-              the Diva Oliva brand are manufactured at the best factories in
-              Italy, Spain, Morocco, Greece and Turkey and meet a high level of
-              quality. A wide assortment of TM Diva Oliva can satisfy the requests
-              of the most demanding gourmet. 
-  
-              Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM
-              Diva Olliva, which literally means "country-side" deserves special
-              attention. This is the best unfiltered olive oil, the recipe of
-              which was passed down from generation to generation by one of the
-              oldest families in Italy. In 2011, the Veld World Itd company
-              acquired the right to the formulation and production of il Paesano
-              butter. The olive pulp inherent in this oll gives it a unique taste
-              and aroma, making it even more useful!`,
-  },
-  {
-    logo: tropicLife,
-    title: "Tropic Life",
-    description: `Tropic Life presents a line of olive oil and olives, an affordable
-              Mediterranean delicacy, a tasty and healthy product. All products of
-              the Diva Oliva brand are manufactured at the best factories in
-              Italy, Spain, Morocco, Greece and Turkey and meet a high level of
-              quality. A wide assortment of TM Diva Oliva can satisfy the requests
-              of the most demanding gourmet. 
-  
-              Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM
-              Diva Olliva, which literally means "country-side" deserves special
-              attention. This is the best unfiltered olive oil, the recipe of
-              which was passed down from generation to generation by one of the
-              oldest families in Italy. In 2011, the Veld World Itd company
-              acquired the right to the formulation and production of il Paesano
-              butter. The olive pulp inherent in this oll gives it a unique taste
-              and aroma, making it even more useful!`,
-  },
-  {
-    logo: veld,
-    title: "Veld World",
-    description: `Veld World presents a line of olive oil and olives, an affordable
-              Mediterranean delicacy, a tasty and healthy product. All products of
-              the Diva Oliva brand are manufactured at the best factories in
-              Italy, Spain, Morocco, Greece and Turkey and meet a high level of
-              quality. A wide assortment of TM Diva Oliva can satisfy the requests
-              of the most demanding gourmet. 
-  
-              Perhaps, 100% natural unfiltered olive oil from Italy il Paesano TM
-              Diva Olliva, which literally means "country-side" deserves special
-              attention. This is the best unfiltered olive oil, the recipe of
-              which was passed down from generation to generation by one of the
-              oldest families in Italy. In 2011, the Veld World Itd company
-              acquired the right to the formulation and production of il Paesano
-              butter. The olive pulp inherent in this oll gives it a unique taste
-              and aroma, making it even more useful!`,
-  },
-];
 
 const BrandsPage: React.FC = () => {
   const location = useLocation();
